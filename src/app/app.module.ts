be824833fb5895183b9146154cb3476ebc4a94ea
DhananjayRaut <asmita.belhekar@gmail.com>
2020-02-08 18:44:45 +0530

    add mterial design plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material'
+import { MatFormFieldModule } from '@angular/material'
 import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
@@ -20,7 +20,7 @@ import { FlexLayoutModule } from '@angular/flex-layout'
      IonicModule.forRoot(),
      AppRoutingModule,
       BrowserAnimationsModule,
-      MatToolbarModule,
+      MatFormFieldModule,
       FlexLayoutModule
     ],
   providers: [
